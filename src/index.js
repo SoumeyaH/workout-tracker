916ae4d658b83dc7e475bff7943f01cc9756c4c5
Soumeya <soumeyahassan47@gmail.com>
2021-07-17 21:49:00 +0100

    added static file path to fix css, added cors as well

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const { connect } = require("./db");
 const routes = require("./routes");
@@ -9,8 +11,10 @@ const app = express();
 
 connect();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../", "public")));
 
 app.use(routes);
 
