91e2980f2e320add8668de982e67fe41a6e27ad8
Soumeya <soumeyahassan47@gmail.com>
2021-07-17 21:22:34 +0100

    added try catch, wip fix css

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -4,15 +4,27 @@ const path = require("path");
 const router = Router();
 
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../public/exercise.html"));
+  try {
+    res.sendFile(path.join(__dirname, "../../../public/exercise.html"));
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../public/stats.html"));
+  try {
+    res.sendFile(path.join(__dirname, "../../../public/stats.html"));
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../../public/index.html"));
+  try {
+    res.sendFile(path.join(__dirname, "../../../public/index.html"));
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 module.exports = router;
