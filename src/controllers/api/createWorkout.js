76598b9163416c84b82d64847fb031687bbd6cf2
Soumeya <soumeyahassan47@gmail.com>
2021-07-18 18:58:49 +0100

    changed to destructing body not keys

--- src/controllers/api/createWorkout.js
+++ src/controllers/api/createWorkout.js
@@ -1,34 +1,8 @@
 const { Workout } = require("../../models");
 
 const createWorkout = async (req, res) => {
-  console.log("create", req.body);
-  const { type, name, weight, sets, reps, duration } = req.body;
+  const { body: workout } = req;
 
-  if (type === "cardio") {
-    const workout = {
-      type,
-      name,
-      distance,
-      duration,
-    };
-    console.log(workout);
-  }
-
-  if (type === "resistance") {
-    const workout = {
-      // to do day: install moment to add current time
-      exercises: [
-        {
-          type,
-          name,
-          weight,
-          sets,
-          reps,
-          duration,
-        },
-      ],
-    };
-  }
   try {
     const newWorkout = await Workout.create(workout);
     res.status(200).json(newWorkout);
