cf1dd95c1fe1ddbff16481b98866f804347b7f22
Soumeya <soumeyahassan47@gmail.com>
2021-07-18 21:39:50 +0100

    tidied up functions

--- src/controllers/api/updateWorkout.js
+++ src/controllers/api/updateWorkout.js
@@ -4,17 +4,14 @@ const updateWorkout = async (req, res) => {
   const { id } = req.params;
   const { body } = req;
 
-  console.log("updated body", body);
-
   try {
     const updatedWorkout = await Workout.findByIdAndUpdate(id, {
       $push: { exercises: body },
     });
 
-    console.log("updatedWorkout", updatedWorkout);
-    res.status(200).json(updatedWorkout);
+    return res.status(200).json(updatedWorkout);
   } catch (err) {
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 };
 
