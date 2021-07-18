2132f7a548354fe3651ba200230a98c99ea903c4
Soumeya <soumeyahassan47@gmail.com>
2021-07-18 16:27:07 +0100

    added get all workouts, wip add sum for distance?

--- src/controllers/api/getAllWorkouts.js
+++ src/controllers/api/getAllWorkouts.js
@@ -1,5 +1,21 @@
-const getAllWorkouts = (req, res) => {
-  res.json({ message: "getAllWorkouts function" });
+const { Workout } = require("../../models");
+
+const getAllWorkouts = async (req, res) => {
+  try {
+    const workouts = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+            // to do - maybe add total distance?
+          },
+        },
+      },
+    ]);
+    res.json(workouts);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 module.exports = getAllWorkouts;
